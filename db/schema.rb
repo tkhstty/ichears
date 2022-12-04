3a54d9cb95049e920bf1420c05fd987f5459ba19
tkhstty <tkhstty@gmail.com>
2022-12-04 16:03:21 +0900

    ideas/index仮作成(表示確認用)
    - ideasテーブル作成
    - ideaモデル、ideasコントローラー作成
      - validatesなどの設定はまだ記述なし
    - routes設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_01_111056) do
+ActiveRecord::Schema.define(version: 2022_12_04_063912) do
+
+  create_table "ideas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_12_01_111056) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ideas", "users"
 end
