a579162263e8d927e1ca3acd7980f2719ebba6a9
tkhstty <tkhstty@gmail.com>
2022-12-07 21:24:22 +0900

    Userモデルの設定
    - validates
    - to ideaのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, uniqueness: true, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/, message: 'is invalid(min 6 characters with alphabets and numbers'}
+  
+  has_many :ideas
 end
